b24c43bd508fc610957e7b02aba6fa594ef236ed
Udit Karode <udit.karode@gmail.com>
2019-02-12 20:56:44 +0530

    fix_phone: remove temporary files after use

--- fix_phone.sh
+++ fix_phone.sh
@@ -17,8 +17,9 @@ su -c mount -o rw,remount /system
 su -c rm -rf /system/SeFix
 su -c mkdir /system/SeFix
 su -c cp helper.sh /system/SeFix/helper.sh
-su -c cp denials /system/SeFix/denials
+su -c mv denials /system/SeFix/denials
 su -c chmod a+x /system/SeFix/helper.sh
+rm sedenials
 echo
 echo "THE PROCESS SHOULD START AND MAY TAKE AS LONG AS 1-2 HOURS."
 echo "PRESS ENTER TO CONTINUE"
