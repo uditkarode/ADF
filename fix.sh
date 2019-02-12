e443b33fad5a43b18610d25810457ea19508880f
Udit Karode <udit.karode@gmail.com>
2019-02-12 20:54:43 +0530

    fix.sh: clear and save only on actual usage

--- fix.sh
+++ fix.sh
@@ -16,17 +16,23 @@ adb shell su -c mount -o rw,remount /system
 adb shell su -c rm -rf /system/SeFix
 adb shell su -c mkdir /system/SeFix
 adb push helper.sh /sdcard/___HSH_
-adb push coms /sdcard/___DENS_
+adb push denials /sdcard/___DENS_
 adb shell su -c mv /sdcard/___HSH_ /system/SeFix/helper.sh
 adb shell su -c mv /sdcard/___DENS_ /system/SeFix/denials
 adb shell su -c chmod a+x /system/SeFix/helper.sh
+if [ ! -z "$(cat denials)" ]; then
+        adb shell su -c logcat -c > /dev/null
+        adb shell su -c supolicy --save /system/SAVED_POLICIES
+        echo "Policies have been saved on your phone at /system/SAVED_POLICIES"
+	echo
+	echo "NOW EXECUTE THESE COMMANDS ON A TERMINAL EMULATOR ON YOUR PHONE:"
+	echo "su"
+	echo "cd /system/SeFix"
+	echo "./helper.sh"
+	echo
+	echo "THE PROCESS SHOULD START AND MAY TAKE AS LONG AS 1-2 HOURS."
+	echo "YOU CAN UNPLUG THE PHONE FROM THE PC NOW."
+else
+	echo "No denials to fix!"
+fi
 rm sedenials denials
-
-echo
-echo "NOW EXECUTE THESE COMMANDS ON A TERMINAL EMULATOR ON YOUR PHONE:"
-echo "su"
-echo "cd /system/SeFix"
-echo "./helper.sh"
-echo
-echo "THE PROCESS SHOULD START AND MAY TAKE AS LONG AS 1-2 HOURS."
-echo "YOU CAN UNPLUG THE PHONE FROM THE PC NOW."
